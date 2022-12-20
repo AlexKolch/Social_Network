1938715bbcd49aa46f48390730093c9648cfa034
Alexey Kolch <kolychenkov94@mail.ru>
2022-12-20 21:39:42 +0400

    LogInView add

---
+++ Navigation/Profile/LogInViewController.swift
@@ -0,0 +1,89 @@
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    let contentView: LogInView = {
+        let contentView = LogInView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.logInButton.addTarget(
+            LogInViewController.self,
+            action: #selector(buttonDidTapped),
+            for: .touchUpInside)
+        return contentView
+    }()
+    
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.contentSize = CGSize(width: .zero, height: UIScreen.main.bounds.height)
+        scrollView.keyboardDismissMode = .interactive
+        return scrollView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        self.view.addSubview(scrollView)
+        setConstraints()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addKeyboardNotifications()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        removeKeyboardNotifications()
+    }
+    
+    func addKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+    
+    func removeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        let userInfo = notification.userInfo
+        let keyboardFrameSize = (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+       // scrollView.contentSize = CGSize(width: .zero, height: keyboardFrameSize.height + 20)
+        scrollView.contentOffset = CGPoint(x: 0, y: keyboardFrameSize.height - self.view.safeAreaInsets.bottom - 200)
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        scrollView.contentOffset = .zero
+    }
+    
+    @objc func buttonDidTapped() {
+        let profileViewController = ProfileViewController()
+        navigationController?.pushViewController(profileViewController, animated: true)
+    }
+}
+
+extension LogInViewController {
+    func setConstraints(){
+        scrollView.addSubview(contentView)
+        scrollView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        scrollView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        contentView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor).isActive = true
+        contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        contentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
+    }
+}
